82a69fc13350bf8b8fa8ccd12f642effe9c63722
MuhamedMagdi <m49dii@gmail.com>
2022-06-07 21:14:40 +0200

    server

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, json } from 'express';
+
+import config from './config';
+import mongoConnect from './database/connect';
+import routes from './routes';
+import { initWorkers } from './utils/reporter';
+
+mongoConnect();
+initWorkers();
+
+const app: Application = express();
+const port: number = config.app.port as number;
+
+app.use(json());
+
+app.use(routes);
+
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}...`);
+});
+
+export default app;
